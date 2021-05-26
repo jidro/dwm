72b1d8413d6224d89909208eb36a1d6ff57db759
Your Name <you@example.com>
2021-05-26 13:27:52 +0800

    change-st-alpha

--- My-dwm-profile/dwm-maker/st/config.def.h
+++ My-dwm-profile/dwm-maker/st/config.def.h
@@ -102,6 +102,12 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.82;
 
+/* bg opacity */
+float alpha = 0.8;
+
+/* bg opacity */
+float alpha = 0.8, alphaUnfocused = 0.6;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
@@ -127,6 +133,10 @@ static const char *colorname[] = {
   /* special colors */
   [256] = "#282a36", /* background */
   [257] = "#f8f8f2", /* foreground */
+
+  /* more colors can be added after 255 to use with DefaultXX */
+  "black",
+
 };
 
 
@@ -135,9 +145,10 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+unsigned int bg = 17, bgUnfocused = 16;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-charact$
--- My-dwm-profile/dwm-maker/st/config.h
+++ My-dwm-profile/dwm-maker/st/config.h
@@ -101,6 +101,9 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.82;
 
+/* bg opacity */
+/* float alpha = 0.8;  */
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
@@ -126,6 +129,10 @@ static const char *colorname[] = {
   /* special colors */
   [256] = "#282a36", /* background */
   [257] = "#f8f8f2", /* foreground */
+
+  /* more colors can be added after 255 to use with DefaultXX */
+  "black",
+
 };
 
 
@@ -134,7 +141,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
