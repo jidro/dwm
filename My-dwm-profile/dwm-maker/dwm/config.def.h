c2edb21e9940f6d6fb7fa252dac5611a65cfce82
Your Name <you@example.com>
2021-05-26 13:39:29 +0800

    change-dwm-fullscreen

--- My-dwm-profile/dwm-maker/dwm/config.def.h
+++ My-dwm-profile/dwm-maker/dwm/config.def.h
@@ -39,9 +39,11 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+        { "M",      monocle },
+/*      { "S",      split },  */
+        { "F",      NULL },    /* no layout function means floating behavior */
+/*      { "B",      bstack },  */
+        { "T",      tile },    /* first entry is default */
 };
 
 /* key definitions */
@@ -74,9 +76,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawm,          {.v = termcmd } },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
