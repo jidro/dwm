9f03d4792343ae629334f3e00c00ec814f0a8837
jidro <z3069566588@163.com>
2021-05-30 15:02:57 +0800

    change-dwm-autostart

--- My-dwm-profile/dwm-maker/scripts/dwm-autostart/wallpapers.sh
+++ My-dwm-profile/dwm-maker/scripts/dwm-autostart/wallpapers.sh
@@ -1,9 +1,5 @@
 #!/bin/env bash
-/bin/bash ~/.dwm/dwm-autostart/scripts.sh &
 
-compton &
-
-#/bin/bash ~/.dwm/dwm-autostart/wallpapers.sh &
 while true; do
 	# ~/Wallpapers是存放壁纸的文件夹
 	feh --recursive --randomize --bg-fill --no-fehbg ~/Wallpapers
