f07e00ef6ac16d0d4c692866003a0d79a8f58778
jidro <61751667+jidro@users.noreply.github.com>
2021-09-09 15:40:28 +0800

    server&xinit

--- dwm-maker/install.sh
+++ dwm-maker/install.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 
 echo "Downloading software please wait..."
-sudo apt install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo pacman -S xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo dnf install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo yum install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo zypper install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo eopkg install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
-sudo xbps-install xorg libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo apt install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo pacman -S xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo dnf install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo yum install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo zypper install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo eopkg install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
+sudo xbps-install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y &>/dev/null
 
 if [ $(ls ~/.xinitrc) = $(ls $HOME/.xinitrc) ];then
 cat ~/.xinitrc .xinitrc > temp && cat temp > ~/.xinitrc && rm -rf temp
