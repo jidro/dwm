3c55d237338db6fc673c9671a2ec4dc743aece08
Jidro.Z <61751667+jidro@users.noreply.github.com>
2022-04-07 11:22:37 +0800

    update install.sh arch

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading software please wait..."
 sudo apt install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower -y
-sudo pacman -S xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower
+sudo pacman -S xorg xorg-server xorg-xinit xorg-xsetroot libx11 libxft libxinerama make cmake feh acpi upower gcc picom ttf-dejavu 
 sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xinit-session libX11-devel libXft-devel libXinerama-devel make cmake upower gcc dejavu-sans-fonts xsetroot g++ picom feh acpi -y
 sudo yum install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xinit-session libX11-devel libXft-devel libXinerama-devel make cmake upower gcc dejavu-sans-fonts -y
 sudo zypper install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower
