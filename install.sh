a8634c32a36d59954331ec239ec4866c942b153e
Jidro.Z <61751667+jidro@users.noreply.github.com>
2022-04-13 13:36:00 +0800

    update install zypper-opensuse

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
 echo "Downloading software please wait..."
-sudo apt install xorg xinit x11-xserver-utils libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower picom gcc g++ fonts-dejavu -y
-sudo pacman -S xorg xorg-server xorg-xinit xorg-xsetroot libx11 libxft libxinerama make cmake feh acpi upower gcc picom ttf-dejavu 
+sudo apt install xorg xinit x11-xserver-utils libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower picom gcc g++ rofi fonts-dejavu -y
+sudo pacman -S xorg xorg-server xorg-xinit xorg-xsetroot libx11 libxft libxinerama make cmake feh acpi upower gcc picom rofi ttf-dejavu 
 sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xinit-session libX11-devel libXft-devel libXinerama-devel make cmake upower gcc dejavu-sans-fonts xsetroot g++ picom feh acpi -y
 sudo yum install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xinit-session libX11-devel libXft-devel libXinerama-devel make cmake upower gcc dejavu-sans-fonts -y
-sudo zypper install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower
+sudo zypper install xorg-x11 xorg-x11-devel xorg-x11-server xinit libX11-devel libXft-devel libXinerama-devel make cmake git feh acpi upower gcc dejavu-fonts wqy-microhei-fonts xsetroot gcc-c++ picom rofi
 sudo eopkg install xorg-server xorg-server-devel xinit libx11-devel libxft-devel libxinerama-devel make cmake feh acpi upower gcc g++dejavu-fonts-ttf xsetroot picom pkg-config automake autoconf libtool
 sudo eopkg install -c system.devel
 sudo xbps-install xorg xorg-server xorg-xinit libx11-dev libxft-dev libxinerama-dev make cmake feh acpi cruft-common upower
